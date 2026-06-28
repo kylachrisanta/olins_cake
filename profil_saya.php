e8af7f7d6bc0c98301fc6376a3523c3a9ef6d1c5
kylachrisanta <kylachrisanta5@gmail.com>
2026-06-28 13:03:38 +0700

    style: add dynamic cache busting to profile page and set visible card borders

--- profil_saya.php
+++ profil_saya.php
@@ -148,7 +148,7 @@ $stmt->close();
     <!-- FontAwesome CDN -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <!-- Global CSS -->
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css?v=<?= time(); ?>">
 </head>
 <body>
 
