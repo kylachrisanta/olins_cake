ae188e575b46102bf576f84dc58f94a6d95e306b
kylachrisanta <kylachrisanta5@gmail.com>
2026-07-03 16:38:23 +0700

    Keamanan: hapus file config sensitif dari tracking Git dan tambahkan ke .gitignore

---
+++ config/fonnte.example.php
@@ -0,0 +1,8 @@
+<?php
+// Konfigurasi Fonnte API
+// Salin file ini menjadi fonnte.php dan isi dengan token asli Anda
+// JANGAN commit fonnte.php ke repository!
+return [
+    'token' => 'MASUKKAN_TOKEN_FONNTE_ANDA_DI_SINI',
+];
+?>
--- config/fonnte.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Konfigurasi Fonnte API
-return [
-    'token' => 'wXYWcDdtDQoADvjipSRz', // Ganti dengan token API Fonnte Anda
-];
-?>
