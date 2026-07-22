0541c436f790373d5ae90bdf9aca51f3eaac9919
kylachrisanta <kylachrisanta5@gmail.com>
2026-07-22 09:11:32 +0700

    fix: add cache-busting query string to style.css in masuk.php and daftar.php and urlencode image path

--- masuk.php
+++ masuk.php
@@ -79,7 +79,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Masuk - Olin's Cake</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css?v=<?= time(); ?>">
 </head>
 <body class="auth-body">
 
